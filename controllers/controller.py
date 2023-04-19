77eff92353039d56799fb9314f4aca5a095ffae8
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-04-19 16:42:43 +0100

    extension partly done

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,11 @@ from app import app
 from flask import render_template
 from models.orders import orders
 
-@app.route('/')
+@app.route('/orders')
 def index():
-    return render_template('index.jinja', orders_list = orders)
+    return render_template('index.html', orders_list = orders)
+
+@app.route('/orders/<index>')
+def order_ind(index):
+    i = int(index) 
+    return render_template('order.html', order=orders[i],index=i)

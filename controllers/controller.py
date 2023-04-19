cc076da89628069a788adbacf899d50e699a5f37
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-04-19 19:17:56 +0100

    working copy

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,11 +7,11 @@ from models.orders import orders
 def index():
     return render_template('index.html', orders_list = orders)
 
-@app.route('/orders/')
+@app.route('/order/')
 def order_list():
     return render_template('order_list.html', orders_list = orders)
     
-@app.route('/orders/<index>')
+@app.route('/order/<index>')
 def order_ind(index):
     i = int(index) 
     return render_template('order.html', order=orders[i],index=i)

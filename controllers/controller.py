c0dbf668398062b50a53b5b75717cc3146c05de5
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-04-19 19:02:53 +0100

    order list page done

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,11 +3,16 @@ from app import app
 from flask import render_template
 from models.orders import orders
 
-@app.route('/orders/')
+@app.route('/')
 def index():
     return render_template('index.html', orders_list = orders)
 
+@app.route('/orders/')
+def order_list():
+    return render_template('order_list.html', orders_list = orders)
+    
 @app.route('/orders/<index>')
 def order_ind(index):
     i = int(index) 
     return render_template('order.html', order=orders[i],index=i)
+
